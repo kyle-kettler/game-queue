42e08870d5492da8ed60b3790b428d5d059476d7
Kyle Kettler <hello@kylekettler.com>
2023-06-13 22:02:27 +0000

    determined structure for local data. #2

--- js/data.js
+++ js/data.js
@@ -1,1 +1,12 @@
 /* exported data */
+const data = [];
+
+// const gameObjectStructure = {
+//   name: 'name',
+//   description: 'description',
+//   background_img: 'url',
+//   want: Boolean,
+//   played: Boolean,
+//   favorite: Boolean,
+//   goodOrBad: Boolean
+// };
